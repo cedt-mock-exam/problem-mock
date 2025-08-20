373db30716d1db280b77708e85481650da15aec0
Watcharawit Pratpiamsuk <sayhi7562@gmail.com>
2025-08-20 23:01:20 +0700

    update genin

--- Resource/genin.cpp
+++ Resource/genin.cpp
@@ -5,28 +5,24 @@ using namespace std;
 
 typedef long long ll;
 
-const ll minn = 10;
-const ll maxn = 2000;
-const ll mina = 1;
-const ll maxa = 1e9;
-
+// ref https://codeforces.com/blog/entry/69370
+// ref https://github.com/MikeMirzayanov/testlib
 // rnd.next(a,b) = random between a-b
 // use cout to show in .in file
 // if you want more tc, just change t<=10
-// after finish , just run file c++
+// after finish , just run this file
 
-int main() {
+int main(int argc, char *argv[]) {
+    registerGen(argc, argv, 1);
+
+    // rnd.setSeed(time(0)); 
+    //if you want to reset tc every run file , just uncomment
+
     for (int t = 1; t <= 10; t++) {
         string tc = "testcase/" + to_string(t) + ".in";
         freopen(tc.c_str(), "w", stdout);
-
-        ll temp = rnd.next(696969);
-        for (int i = 1; i < temp; i++) {
-            rnd.next(i);
-            rnd.next(i, i);
-        }
-
         //you gen code
+        
     }
     return 0;
 }
