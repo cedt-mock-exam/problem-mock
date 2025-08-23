fa14841a450348b5960f2338501e4254ecf44352
wrxzzox <porkuang2548@gmail.com>
2025-08-23 17:35:22 +0700

    cm1

--- comprog-midterm/cm1/cm1-genin.cpp
+++ comprog-midterm/cm1/cm1-genin.cpp
@@ -3,11 +3,17 @@ using namespace std;
 
 int main() {
     srand(time(NULL));
-    for(int in=1; in<=20; in++) {
+    for(int in=1; in<=6; in++) {
         ofstream fout;
         fout.open(to_string(in)+".in");
-        int n = rand()%1000000000,m = rand()%1000000000;
+        int n = rand()%10000000,m = rand()%10000000+n;
         fout << n << " "<<m;
+    }
+    for(int in=7; in<=20; in++) {
+        ofstream fout;
+        fout.open(to_string(in)+".in");
+        int n = rand()%100000000,m = rand()%100000000+n;
+        fout << m << " "<<n;
     }
     return 0;
 }
