7bee133a4e6a4583b9f2cdc3b3c25fb2ad52f5b1
wrxzzox <porkuang2548@gmail.com>
2025-08-26 15:54:13 +0700

    Update cm1-sol.cpp

--- comprog-midterm/cm1/cm1-sol.cpp
+++ comprog-midterm/cm1/cm1-sol.cpp
@@ -35,11 +35,7 @@ int main()
             c += a / 5 * 2;
             d += a / 7 * 3;
             tmp *= tmp*3 / 2;
-            if (tmp / 2 >= tw)
-            {
-                tmp = tw + th;
-                break;
-            }
+            if (tmp / 2 >= tw) break;
         }
     }
     else
@@ -49,13 +45,8 @@ int main()
             b += a / 3;
             c += a / 5;
             d += a / 7;
-            if (a * b < b * c)
-            {
-                tmp = tw + th;
-                break;
-            }
-            else
-                tmp -= (a - b - c) / 2;
+            if (a * b < b * c) break;
+            else tmp -= (a - b - c) / 2;
         }
     }
 while (d < b + c)
