a5452b04c15a970f3fcf5279d5796770819fd4f0
phone64bit <iamphoneza@gmail.com>
2025-08-26 22:54:51 +0700

    Change variable K to Z

--- comprog-midterm/cm2/cm2-gensol.cpp
+++ comprog-midterm/cm2/cm2-gensol.cpp
@@ -7,8 +7,8 @@ void solve(string fname) {
     fin.open(fname+".in");
     fout.open(fname+".sol");
     int n = 0, xi = 0;
-    double x_sum = 0, x_sum2 = 0, x_avg = 0, sd = 0, k = 0, ans = 0;
-    fin >> k;
+    double x_sum = 0, x_sum2 = 0, x_avg = 0, sd = 0, z = 0, ans = 0;
+    fin >> z;
     while(fin >> xi && xi != -1) {
         x_sum += xi;
         x_sum2 += xi*xi;
@@ -16,7 +16,7 @@ void solve(string fname) {
     }
     x_avg = x_sum / n;
     sd = sqrt( (x_sum2/n) - (x_avg*x_avg) );
-    ans = k*sd + x_avg;
+    ans = z*sd + x_avg;
     fout << fixed << setprecision(5) << ans;
 }
 
--- comprog-midterm/cm2/cm2-sol.cpp
+++ comprog-midterm/cm2/cm2-sol.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 int n, xi;
-double x_sum, x_sum2, x_avg, sd, k, ans;
+double x_sum, x_sum2, x_avg, sd, z, ans;
 
 int main() {
-    cin >> k;
+    cin >> z;
     while(cin >> xi && xi != -1) {
         x_sum += xi;
         x_sum2 += xi*xi;
@@ -15,7 +15,7 @@ int main() {
     }
     x_avg = x_sum / n;
     sd = sqrt( (x_sum2/n) - (x_avg*x_avg) );
-    ans = k*sd + x_avg;
+    ans = z*sd + x_avg;
     cout << fixed << setprecision(5) << ans;
     return 0;
 }
