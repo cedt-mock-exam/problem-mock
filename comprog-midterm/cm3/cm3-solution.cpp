cb724801353675516a49a4062e190935e99d3266
DayKungZa <tawanday2006@gmail.com>
2025-08-26 21:21:48 +0700

    added cm3

---
+++ comprog-midterm/cm3/cm3-solution.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //input size of paper and no. of balls
+	int n, m, k;
+	cin >> n >> m >> k;
+	char map[n][m];
+	
+	//write all tiles as '-'
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++)
+			map[i][j] = '-';
+	}
+	
+    //loop for each ball
+	while(k--){
+        //input each ball's color, radius, and position
+		char c;
+		int r, x, y;
+		cin >> c >> r >> x >> y;
+
+        //loop from x-r to x+r (rows)
+		for(int i=x-r; i<=x+r; i++){
+            //in each row have a loop to draw colors in columns satisfying the condition
+			for(int j=y-(r-abs(x-i)); j<=y+(r-abs(x-i)); j++){
+				if(!(i<0||j<0||i>=n||j>=m))
+					map[i][j] = c;
+			}
+		}
+	}
+	
+    //print answer
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++)
+			cout << map[i][j] << ' ';
+		cout << '\n';
+	}		
+}
