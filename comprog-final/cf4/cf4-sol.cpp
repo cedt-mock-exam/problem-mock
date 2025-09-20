89c9ec44fb15e35df05f1d3a8ad2991ecadad25d
Palm •-• <ch.enderpalm@gmail.com>
2025-09-21 01:10:04 +0700

    doc: add comments to fc4-sol

--- comprog-final/cf4/cf4-sol.cpp
+++ comprog-final/cf4/cf4-sol.cpp
@@ -37,17 +37,16 @@ int main() {
       i--; // no need to erase merge child (testcase will not call it anyway)
       continue;
     }
-    // normal flight command
+
     string dest;
     cin >> dest;
 
-    // string parsing time!!
     string src_code = src.substr(0, 3);
     int src_gate = stoi(src.substr(4, src.length() - 5));
     string dest_code = dest.substr(0, 3);
     int dest_gate = stoi(dest.substr(4, dest.length() - 5));
 
-    airports[src_code][src_gate] = false;
+    airports[src_code][src_gate] = false; // free src gate first
     if (!airports[dest_code][dest_gate]) { // trivial case
       airports[dest_code][dest_gate] = true;
       continue;
@@ -56,7 +55,6 @@ int main() {
     int left = -1, right = -1;
     int gate_size = airports[dest_code].size();
 
-    // Find left gate - start from j=1 to skip the occupied target gate
     for (int j = 1; dest_gate - j >= 0; j++) {
       if (!airports[dest_code][dest_gate - j]) {
         left = dest_gate - j;
@@ -64,7 +62,6 @@ int main() {
       }
     }
 
-    // Find right gate - start from j=1 to skip the occupied target gate
     for (int j = 1; dest_gate + j < gate_size; j++) {
       if (!airports[dest_code][dest_gate + j]) {
         right = dest_gate + j;
@@ -73,24 +70,24 @@ int main() {
     }
 
     int sol = -1;
-    if (left != -1 && right == -1) {
-      sol = left;
-    } else if (left == -1 && right != -1) {
-      sol = right;
+    if (left != -1 && right == -1) { 
+      sol = left; // right reach the end while left not
+    } else if (left == -1 && right != -1) { 
+      sol = right; // left reach the end while right not
     } else if (left != -1 && right != -1) {
       if (abs(dest_gate - left) < abs(dest_gate - right)) {
-        sol = left;
+        sol = left; // left is nearer
       } else if (abs(dest_gate - left) > abs(dest_gate - right)) {
-        sol = right;
+        sol = right; // right is nearer
       } else {
-        sol = left;
+        sol = left; // if both is equally far, choose left
       }
     }
     if (sol != -1) {
-      airports[dest_code][sol] = true;
+      airports[dest_code][sol] = true; // able to land, dest gate is occupied
       cout << i << " SHIFT " << dest_code << '[' << sol << "]\n";
     } else {
-      airports[src_code][src_gate] = true;
+      airports[src_code][src_gate] = true; // unable to land, fly back to src gate
       cout << i << " CANCEL\n";
     }
   }
